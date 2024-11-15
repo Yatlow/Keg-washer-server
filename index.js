c2947bbb8de53fd7bd62459492a5afd28a970cf7
Yatlow <82391403+Yatlow@users.noreply.github.com>
2024-11-16 00:01:41 +0200

    Update route .js

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ app.use(express.json());
         res.status(403).json({ message: 'Token verification failed' });
         console.log("auth faild")
     }
-
+    };
 
 
 app.post('/update', verifyToken, async (req, res) => {
